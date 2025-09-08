d6245604e261b3a7db158ec90889e33ebe4b0e6e
Sergie Code <sergiosnsdk@gmail.com>
2025-09-08 13:21:17 -0500

    Clases asignación variables y correcta escritura

---
+++ variables/variables.py
@@ -0,0 +1,20 @@
+x = "Esta es una variable"
+print(x)
+
+X = "Esta es OTRA variable"
+print(x)
+
+mivariable = "Esta es una variable"
+mi_variable = "Esta es una variable con guión bajo"
+_mi_variable = "Esta variable comienza con un guión bajo"
+miVariable = "Otra varible"
+MIVARIABLE = "Todo mayus"
+mivariable2 = "Otra variable más"
+
+2variable = "No se puede utilizar esta forma"
+mi-variable = "No se puede utilizar guión medio"
+mi variable = "No se puede utilizar espacios"
+
+camelCase = "Comienza con minúscula y el resto de las palabras con mayúscula"
+PascalCase = "Comienza con mayúscula y el resto de las palabras también siguen en mayúscula"
+snake_case = "Se separan las palabras con guiones bajos"
