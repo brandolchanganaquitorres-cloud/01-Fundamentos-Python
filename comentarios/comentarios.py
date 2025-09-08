943017b7516898956391514a5d4b66f7cdf7c41b
Sergie Code <sergiosnsdk@gmail.com>
2025-09-08 12:53:25 -0500

    Clase comentarios

---
+++ comentarios/comentarios.py
@@ -0,0 +1,16 @@
+# Esta es una linea de código
+print("Hola mundo") # Imprime hola mundo
+
+# Este
+# es
+# un
+# comentario
+
+"""
+Este
+será 
+un comentario
+multilinea
+"""
+
+# print("Esta linea deseo ignorarla")
