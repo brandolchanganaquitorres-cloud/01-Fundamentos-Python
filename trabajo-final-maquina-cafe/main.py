ae055dba892300ea0f715bb9a7ddc2ab23e42ca0
Sergie Code <sergiosnsdk@gmail.com>
2025-09-11 09:07:22 -0500

    Clase trabajo final parte 2

--- trabajo-final-maquina-cafe/main.py
+++ trabajo-final-maquina-cafe/main.py
@@ -1,4 +1,6 @@
 from menu import mostrar_menu
+from pedidos import pedir_cafe
+from historial import ver_historial
 
 def main():
     while True:
@@ -6,11 +8,9 @@ def main():
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
-            #pedir un cafe
-            pass
+            pedir_cafe()
         elif opcion == "2":
-            # ver el historial
-            pass
+            ver_historial()
         elif opcion == "3":
             print("\n Muchas gracias por haber tomado nuestros riquísimos cafés")
             break
--- trabajo-final-maquina-cafe/pedidos.py
+++ trabajo-final-maquina-cafe/pedidos.py
@@ -1,3 +1,4 @@
+ARCHIVO_PEDIDOS = "pedidos.txt"
 
 def pedir_cafe():
     print("\n Elige el café que prefieras: ")
@@ -14,3 +15,12 @@ def pedir_cafe():
         "3": "Latte",
         "4": "Americano"
     }
+
+    if opcion in cafes:
+        cafe_elegido = cafes[opcion]
+        print("Has pedido un " + cafe_elegido + ". ¡Preparando tu café!")
+
+        with open(ARCHIVO_PEDIDOS, "a", encoding="utf-8") as archivo:
+            archivo.write(cafe_elegido + "\n")
+    else:
+        print("Opción no es válida por favor intenta de nuevo")
